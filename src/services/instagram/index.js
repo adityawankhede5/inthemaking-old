89c03c05a4e69bd50446b892dac99df6af14c7d9
adityawankhede5 <adityawankhede555@gmail.com>
2021-11-25 23:18:10 +0530

    typo fix

--- src/services/instagram/index.js
+++ src/services/instagram/index.js
@@ -1,7 +1,7 @@
 export const initAuthFlow = () => {
     const apiBaseUrl = "https://api.instagram.com/";
     const data = {
-        "app_id": process.env.REACT_APP_INSTAGRAM_APP_ID,
+        "client_id": process.env.REACT_APP_INSTAGRAM_APP_ID,
         "redirect_uri": process.env.REACT_APP_INSTAGRAM_REDIRECT_URI,
         "scope": "user_profile,user_media",
         "response_type": "code"
@@ -12,8 +12,9 @@ export const initAuthFlow = () => {
         queryParams.append(item, data[item]);
     }
     
-    const authorizationUrl = apiBaseUrl + "oatuth/authorize?" + queryParams.toString();
-    const link = document.createElement("a");
-    link.href = authorizationUrl;
-    link.click();
+    const authorizationUrl = apiBaseUrl + "oauth/authorize?" + queryParams.toString();
+    console.log(authorizationUrl);
+    // const link = document.createElement("a");
+    // link.href = authorizationUrl;
+    // link.click();
 }
