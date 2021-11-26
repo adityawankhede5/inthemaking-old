50bc67ee4e252f52089449496263f9419fba4e74
adityawankhede5 <adityawankhede555@gmail.com>
2021-11-26 19:35:26 +0530

    login with instagram working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import { initAuthFlow } from './services/instagram';
+import { getAccessToken, initAuthFlow } from './services/instagram';
 
 function App() {
+  const queryParams = new URLSearchParams(window.location.search);
+  const code = queryParams.get("code");
   return (
     <div className="App">
       <button onClick={initAuthFlow}>Init auth flow</button>
+      {
+        code &&
+        <div>
+        <div>{code}</div>
+        <button onClick={() => {getAccessToken(code)}}>Get Access Token</button>
+        </div>
+      }
     </div>
   );
 }
--- src/services/instagram/index.js
+++ src/services/instagram/index.js
@@ -1,5 +1,7 @@
+import axios from "axios";
+
+const apiBaseUrl = "https://api.instagram.com/";
 export const initAuthFlow = () => {
-    const apiBaseUrl = "https://api.instagram.com/";
     const data = {
         "client_id": process.env.REACT_APP_INSTAGRAM_APP_ID,
         "redirect_uri": process.env.REACT_APP_INSTAGRAM_REDIRECT_URI,
@@ -13,8 +15,57 @@ export const initAuthFlow = () => {
     }
     
     const authorizationUrl = apiBaseUrl + "oauth/authorize?" + queryParams.toString();
-    console.log(authorizationUrl);
-    // const link = document.createElement("a");
-    // link.href = authorizationUrl;
-    // link.click();
+    // console.log(authorizationUrl);
+    const link = document.createElement("a");
+    link.href = authorizationUrl;
+    link.click();
 }
+
+export const getAccessToken = (code) => {
+    const data = {
+        "client_id": process.env.REACT_APP_INSTAGRAM_APP_ID,
+        "client_secret": process.env.REACT_APP_INSTAGRAM_SECRET,
+        "grant_type": "authorization_code",
+        "redirect_uri": process.env.REACT_APP_INSTAGRAM_REDIRECT_URI,
+        code: code
+    }
+    const formData = new FormData();
+    for(let item in data){
+        formData.append(item, data[item]);
+    }
+    const tokenUrl = apiBaseUrl + "oauth/access_token";
+    
+    return axios.post(tokenUrl, formData).then(resp => {
+        const {access_token, user_id} = resp.data;
+        localStorage.setItem("instagram", JSON.stringify({access_token, user_id}));
+        window.location.replace(window.location.origin + window.location.pathname);
+    }).catch(err => {
+        console.error(err)
+        window.location.replace(window.location.origin + window.location.pathname);
+    })
+    
+}
+
+// export const getLongLivedAccessToken = async (code) => {
+//     let {access_token, user_id}  = await getAccessToken(code);
+//     if(!access_token) return {success: false};
+//     console.log({access_token, user_id})
+//     const data = {
+//         "grant_type": "ig_exchange_token",
+//         "client_secret": process.env.REACT_APP_INSTAGRAM_SECRET,
+//         "access_token": access_token
+//     }
+//     const queryParams = new URLSearchParams();
+//     for(let item in data){
+//         queryParams.append(item, data[item]);
+//     }
+//     const tokenUrl = "https://graph.instagram.com/access_token" + queryParams.toString();
+//     axios.get(tokenUrl).then(resp => {
+//         console.log(resp);
+//         let {access_token, token_type, expires_in} = resp.data;
+//         console.log({access_token, token_type, expires_in});
+//     }).catch(err => {
+//         console.error(err)
+//     })
+
+// }
